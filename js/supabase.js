dd9cd8a13cb74b001509788c921e3163c8731c96
Rick45Patrick <patrickmwanzia136@gmail.com>
2026-07-29 16:02:50 +0300

    Refactor Supabase connection code

--- js/supabase.js
+++ js/supabase.js
@@ -1,29 +1,17 @@
-// ==========================================
-// GRADE HUB - SUPABASE CONNECTION
-// ==========================================
-
-
-// Import Supabase client
-
 import { createClient } 
 from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 
-// Your Supabase project URL
-
-const supabaseUrl = 
+const supabaseUrl =
 "https://vpvwvtpbrkrrlezwkpup.supabase.co";
 
 
-// Your Supabase anon public key
-
-const supabaseKey = 
+const supabaseKey =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdnd2dHBicmtycmxlendrcHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM0ODMsImV4cCI6MjEwMDg5OTQ4M30.4Sq3G2P6LYH2nXgQxTdDG8tP39F0Mbp34m7AfaaqrcE";
 
 
-// Create Supabase connection
-
-export const supabase = createClient(
+export const supabase =
+createClient(
     supabaseUrl,
     supabaseKey
 );
