ce64b8de07bbfa4917c1215210c54ef4d1b9aa09
Rick45Patrick <patrickmwanzia136@gmail.com>
2026-07-31 12:10:00 +0300

    Update Supabase client initialization variables

--- js/supabase.js
+++ js/supabase.js
@@ -1,17 +1,17 @@
-import { createClient } 
-from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from
+    "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 
-const supabaseUrl =
-"https://vpvwvtpbrkrrlezwkpup.supabase.co";
+const SUPABASE_URL =
+    "https://vpvwvtpbrkrrlezwkpup.supabase.co";
 
 
-const supabaseKey =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdnd2dHBicmtycmxlendrcHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM0ODMsImV4cCI6MjEwMDg5OTQ4M30.4Sq3G2P6LYH2nXgQxTdDG8tP39F0Mbp34m7AfaaqrcE";
+const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdnd2dHBicmtycmxlendrcHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM0ODMsImV4cCI6MjEwMDg5OTQ4M30.4Sq3G2P6LYH2nXgQxTdDG8tP39F0Mbp34m7AfaaqrcE";
 
 
 export const supabase =
-createClient(
-    supabaseUrl,
-    supabaseKey
-);
+    createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
